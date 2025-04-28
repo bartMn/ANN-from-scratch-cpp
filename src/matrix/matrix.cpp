94a4079e01c9c1e8da0c0b7cfd2926c0289317bc
bartMn <bartoszmania.bm+github@gmail.com>
2025-04-28 20:00:43 +0200

    updated matrix creator and tests, added printMatrix function

--- src/matrix/matrix.cpp
+++ src/matrix/matrix.cpp
@@ -1,13 +1,35 @@
 #include "matrix.h"
 #include <iostream>
 
-void Matrix::test(){
-    std::cout << this->rows << std::endl;
+Matrix::Matrix(int r, int c, float* mat){
+    rows = r;
+    columns = c;
+    matrix_vals.resize(c, std::vector<float>(r, 0.0f));
+
+    for (int row=0; row < r; row++){
+        for (int col=0; col< c; col++){
+            matrix_vals[row][col] = *((mat + row * c) + col);
+        }
+    }
+    //matrix_vals[0][0] = 0.0;
 }
 
-int Matrix::get_rows_num(){
-    return this -> rows;
+Matrix::Matrix(int r, int c){
+    rows = r;
+    columns = c;
+    matrix_vals.resize(c, std::vector<float>(r, 0.0f));
 }
-int Matrix::get_columns_num(){
-    return this -> columns;
+
+
+void Matrix::printMatrix(){
+    for (int r=0; r < this->rows; r++){
+        for (int c=0;c < this->columns; c++){
+            std::cout << matrix_vals[r][c]<< " ";
+        }
+        std::cout << std::endl;
+    }
 }
+
+int Matrix::get_rows_num(){ return this -> rows; } 
+int Matrix::get_columns_num(){ return this -> columns; }
+float Matrix::get_val(int row, int col) {return this-> matrix_vals[row][col]; }
--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -1,18 +1,39 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+#include <vector>
 
 class Matrix{
     public:
-        Matrix(int r, int c): rows(r), columns(c) {}
-        void test();
+        Matrix(int r, int c, float* mat);
+        Matrix(int r, int c);
         int get_rows_num();
         int get_columns_num();
-        
+        void printMatrix();
+        float get_val(int row, int col);
+
+    /*
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+    Matrix& operator*=(double scalar);
+    Matrix& operator/=(double scalar);
+    */
 
     private:
         int rows;
         int columns;
+        std::vector<std::vector<float>> matrix_vals;
 };
+/*
+    
+// Vector operators
+Matrix operator+(const Matrix& a, const Matrix& b);
+Matrix operator-(const Matrix& a, const Matrix& b);
+Matrix operator*(const Matrix& v, double scalar);
+Matrix operator*(const Matrix& a, const Matrix& b);
+Matrix operator*(double scalar, const Matrix& v);
+Matrix operator/(const Matrix& v, double scalar);
+Matrix operator-(const Matrix& v);
 
+*/
 
 #endif
