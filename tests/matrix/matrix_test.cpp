e490bba27b03562b5303de8724335f7403bfebb8
bartMn <bartoszmania.bm+github@gmail.com>
2025-04-28 20:23:25 +0200

    updated unit tests

--- tests/matrix/matrix_test.cpp
+++ tests/matrix/matrix_test.cpp
@@ -11,7 +11,7 @@ int func1(int c, const std::string& m)
     return 0;
 }
 
-void matrix_test1()
+int matrix_test1()
 {
     std::cout << "matrix.cpp imported from src OK\n";
     std::thread t1(func1, 1, "mes1\n");
@@ -21,56 +21,79 @@ void matrix_test1()
     t2.join();
     Matrix test_m(666, 555);
     std::cout << "The maxtix has " << test_m.get_rows_num() << " rows and " << test_m.get_columns_num() << " columns" << std::endl;
-    //return 0;
+    return 0;
 }
 
-void test_constructor() {
+int test_constructor() {
+    int status = 0;
     Matrix m(3, 5);
-    assert(m.get_rows_num() == 3);
-    assert(m.get_columns_num() == 5);
+    if (m.get_rows_num() != 3) status = -1;
+    if (m.get_columns_num() != 5) status = -1;
+
+    if (status == -1){
+        std::cout << "test_constructor Failed.\n";
+        return -1;
+    }
 
     float arr[2][3] = {{1.0,2.0,3.0}, {4.0,5.0,6.0}};
     Matrix m1(2, 3, *arr);
 
     // Check dimensions
-    assert(m1.get_rows_num() == 2);
-    assert(m1.get_columns_num() == 3);
-    assert(m1.get_val(0, 0) == 1.0f);
-    assert(m1.get_val(0, 1) == 2.0f);
-    assert(m1.get_val(0, 2) == 3.0f);
-    assert(m1.get_val(1, 0) == 4.0f);
-    assert(m1.get_val(1, 1) == 5.0f);
-    assert(m1.get_val(1, 2) == 6.0f);
+    if (m1.get_rows_num() != 2) status = -1;
+    if (m1.get_columns_num() != 3) status = -1;
+    if (m1.get_val(0, 0) != 1.0f) status = -1;
+    if (m1.get_val(0, 1) != 2.0f) status = -1;
+    if (m1.get_val(0, 2) != 3.0f) status = -1;
+    if (m1.get_val(1, 0) != 4.0f) status = -1;
+    if (m1.get_val(1, 1) != 5.0f) status = -1;
+    if (m1.get_val(1, 2) != 6.0f) status = -1;
 
-    std::cout << "test_constructor passed.\n";
+    if (status == 0) std::cout << "test_constructor passed.\n"; 
+    else std::cout << "test_constructor Failed.\n";
+    return status;
 }
 
-void test_print_matrix() {
+int test_print_matrix() {
     float arr[2][3] = {{1.0,2.0,3.0}, {4.0,5.0,6.0}};
     Matrix m(2, 3, *arr);
     m.printMatrix();
     std::cout << "test_constructor passed.\n";
+    return 0;
 }
 
-void test_get_rows_num() {
+int test_get_rows_num() {
     Matrix m(7, 2);
-    assert(m.get_rows_num() == 7);
+    if (m.get_rows_num() != 7){
+        std::cout << "test_get_rows_num Falied.\n";
+        return -1;
+    }
     std::cout << "test_get_rows_num passed.\n";
+    return 0;
 }
 
-void test_get_columns_num() {
+int test_get_columns_num() {
     Matrix m(4, 9);
-    assert(m.get_columns_num() == 9);
+    if (m.get_columns_num() != 9){
+        std::cout << "test_get_columns_num Failed.\n";
+        return -1;
+    }
     std::cout << "test_get_columns_num passed.\n";
+    return 0;
 }
 
 
 int run_matrix_tests() {
-    test_constructor();
-    test_get_rows_num();
-    test_get_columns_num();
-    test_print_matrix();
+    int status = 0;
+    if (test_constructor() != 0) status = -1;
+    if (test_get_rows_num() != 0) status = -1;
+    if (test_get_columns_num() != 0) status = -1;
+    if (test_print_matrix() != 0) status = -1;
 
-    std::cout << "All tests passed!\n";
+    if (status == 0) {
+        std::cout << "All matrix tests passed successfully!\n";
+    } else {
+        std::cerr << "Some matrix tests failed.\n";
+    }
+
     return 0;
 }
--- tests/matrix/matrix_test.h
+++ tests/matrix/matrix_test.h
@@ -1,2 +1,2 @@
-void matrix_test1();
+int matrix_test1();
 int run_matrix_tests();
