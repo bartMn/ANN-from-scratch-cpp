a9b5ff5533f5bc1d89cc6011e5581165221ddabe
bartMn <bartoszmania.bm+github@gmail.com>
2025-05-29 20:25:59 +0200

    Added a test for one data sample

--- tests/ann/ann_test.cpp
+++ tests/ann/ann_test.cpp
@@ -37,6 +37,25 @@ int test_calcualte_loss() {
     return 0;
 }
 
+int test_one_sample_training() {
+    ANN ann({2, 10, 2}, {"ReLu", "linear"});
+    float v[2][1] = {{1.0}, {2.0}}; 
+    Matrix input(2, 1, *v);
+    Matrix target(2, 1, *v);
+    
+    for (int ct = 0; ct < 10; ct++) {
+        std::cout << "Training iteration: " << ct << "\n";
+        ann.forward(input);
+        ann.reset_gradients(); // Reset gradients before each training step
+        ann.calcualte_loss(target);
+        ann.backprop();
+        ann.update_weights(); // Update weights after backpropagation
+    }
+
+    std::cout << "test_one_sample_training passed.\n";
+    return 0;
+}
+
 
 
 int run_ann_tests() {
@@ -45,6 +64,7 @@ int run_ann_tests() {
     if (test_forward() != 0) status = -1;
     if (test_backprop() != 0) status = -1;
     if (test_calcualte_loss() != 0) status = -1;
+    if (test_one_sample_training() != 0) status = -1;
     //std::this_thread::sleep_for(std::chrono::seconds(1));
     if (status == 0) {
         std::cout << "All ANN tests passed successfully!\n";
